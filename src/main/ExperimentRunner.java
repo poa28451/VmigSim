cf652cb645fc92d4a962e1a8de773f7f25f7230f
Taweesin Wongruschatapokai <taweesin_poa@hotmail.com>
2015-02-06 01:29:05 +0700

    Check if the output directory is existed
    - เพิ่มโค้ดการสร้าง directory หากไม่มีอยู่ก่อนแล้วเข้าไปใน
    FilePathContainer
    - เปลี่ยนเวอร์ชั่น compiler เป็น 1.8

--- src/main/ExperimentRunner.java
+++ src/main/ExperimentRunner.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
@@ -38,8 +37,6 @@ public class ExperimentRunner {
 	private JsonDecoder decoder;
 	
 	public ExperimentRunner(String inputPath, String outputPath, int experimentRounds){
-		/*File out = new File(outputPath);
-		out.mkdirs();*/
 		FilePathContainer.setInputPath(inputPath);
 		FilePathContainer.setOutputPath(outputPath);
 		decoder = new JsonDecoder();
--- src/variable/FilePathContainer.java
+++ src/variable/FilePathContainer.java
@@ -1,5 +1,7 @@
 package variable;
 
+import java.io.File;
+
 public class FilePathContainer {
 	public static String inputPath;
 	public static String outputPath;
@@ -10,8 +12,23 @@ public class FilePathContainer {
 	}
 	public static void setOutputPath(String outputPath) {
 		FilePathContainer.outputPath = outputPath;
+		checkIfDirectoryExist(outputPath);
 	}
 	public static void setNetworkPath(String networkPath) {
 		FilePathContainer.networkPath = networkPath;
+		checkIfDirectoryExist(networkPath);
+	}
+	
+	private static void checkIfDirectoryExist(String filePath){
+		int pos1 = filePath.lastIndexOf('/');
+		int pos2 = filePath.lastIndexOf('\\');
+		
+		//If both was -1, it means filePath has no directory included
+		if(pos1 != -1 && pos2 != -1){
+			int usedPos = pos1 > pos2 ? pos1 : pos2;
+			String dirPath = filePath.substring(0, usedPos);
+			File dir = new File(dirPath);
+			dir.mkdirs();
+		}
 	}
 }
